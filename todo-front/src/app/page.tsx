d34f0db75acaecd9a1ca44b14ebe47082520f589
borkoloco <santiagobosca@gmail.com>
2024-12-24 05:37:25 -0300

    list

--- todo-front/src/app/page.tsx
+++ todo-front/src/app/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "../components/TodoList";
 export default function Home() {
   return (
     <div>
-      <h1>To-Do App</h1>
+      <h1>To-Do List</h1>
       <TodoForm />
       <TodoList />
     </div>
